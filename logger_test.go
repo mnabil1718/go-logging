d63591b5d01591f548e26c3b4bf0c8a34ffc94cc
tingkatin <muhammad.nabil@superindo.co.id>
2024-06-06 10:26:14 +0700

    Added log hook to fire

--- logger_test.go
+++ logger_test.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"errors"
+	"fmt"
 	"os"
 	"testing"
 
@@ -51,4 +53,43 @@ func TestLoggerWithField(t *testing.T) {
 		"role":       "admin",
 		"authorized": false,
 	}).Warn("User unauthorized login")
+}
+
+type LogHook struct {
+}
+
+// list of log levels that would trigger fire
+func (hook *LogHook) Levels() []logrus.Level {
+	return []logrus.Level{logrus.WarnLevel, logrus.ErrorLevel}
+}
+
+func (hook *LogHook) Fire(entry *logrus.Entry) error {
+	if entry.Level == logrus.ErrorLevel {
+		fmt.Println("ERROR: ", entry.Message)
+		return errors.New("log error: " + entry.Message)
+	}
+	fmt.Println("WARNING: ", entry.Message)
+	return nil
+}
+
+func TestLogHook(t *testing.T) {
+	log := logrus.New()
+
+	// create logs dir first
+	err := os.MkdirAll("./logs/", 0755)
+	if err != nil {
+		panic(err)
+	}
+
+	file, err := os.OpenFile("./logs/application.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+	if err != nil {
+		panic(err)
+	}
+
+	log.SetOutput(file)
+	log.AddHook(&LogHook{})
+
+	log.Info("Log outputted to log file")
+	log.Warn("Unauthorized login detected")
+	log.Error("Internal Server Error")
 }
